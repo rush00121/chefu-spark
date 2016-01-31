7455c40a5e6983657cf000e9f832309abc23bc7b
Rushabh <rush00121@gmail.com>
2016-01-31 14:24:32 -0800

    Initial commit

--- src/main/java/com/chefu/App.java
+++ src/main/java/com/chefu/App.java
@@ -5,6 +5,7 @@ import spark.Response;
 import spark.Route;
 
 import static spark.Spark.get;
+import static spark.Spark.port;
 
 /**
  * Created by rushabh on 1/31/16.
@@ -12,11 +13,20 @@ import static spark.Spark.get;
 public class App {
 
     public static void main(String[] args) {
+       port(getHerokuAssignedPort());
         get("/", new Route() {
             @Override
             public Object handle(Request request, Response response) {
                 return "Hello World!!";
             }
         });
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
